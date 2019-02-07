144e40fcb189a27dab9fb7fac4e8b7740949bdde
Raghav Khandelwal <raghav.khandelwal@quantiphi.com>
2019-02-07 14:28:58 +0530

    Add docstring and correct fatal bug

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+'''
+This script calls the Jenkins API on the specified ENDPOINT to list all active jobs, with status of the last succesful build of a specified job.
+The client API library used is [jenkinsapi](https://github.com/pycontribs/jenkinsapi).
+Installation: pip install jenkinsapi
+Documentation: https://jenkinsapi.readthedocs.io/en/latest/
+'''
+
 from jenkinsapi.jenkins import Jenkins
 
 TOKEN = '113941e94f1f89c7153d965cfab80d6c70'
@@ -6,15 +13,14 @@ JOBNAME = 'ao-raghav-test'
 
 J = Jenkins(ENDPOINT, username='teraflik', password=TOKEN)
 
-job = J[JOBNAME]
-lgb = job.get_last_good_build()
+print ("===== ALL JOBS =====")
+for index, job in enumerate(J.get_jobs()):
+    print ("{:d}-".format(index+1))
+    print ('Name: %s' %job[1].name)
+    print ('Description: %s' %job[1].get_description())
+    print ('Running: %s' %job[1].is_running())
+    print ('Enabled: %s' %job[1].is_enabled())
 
-for j in J.get_jobs():
-    job = J[JOBNAME]
-    print ('Job Name:%s' %(job_instance.name))
-    print ('Job Description:%s' %(job_instance.get_description()))
-    print ('Is Job running:%s' %(job_instance.is_running()))
-    print ('Is Job enabled:%s' %(job_instance.is_enabled()))
-
-print("Last good job: {}".format(lgb))
-print("Successful: {}".format(lgb.is_good()))
+test_job = J[JOBNAME]
+lgb = test_job.get_last_good_build()
+print("\nLast good build for job '{}': {}".format(JOBNAME, lgb))
